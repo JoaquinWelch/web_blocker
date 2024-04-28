42f452dd0ea6016cc5303518f65a48555d0841eb
Joaquin Welch <joaquinwelch510@gmail.com>
2024-04-27 23:29:49 -0500

    GUI

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk()
+heading=Label(window, text ='Website Blocker' , font ='arial')
+heading.pack()
+window.geometry('650x400')
+window.minsize(650,400)
+window.maxsize(650,400)
+window.title("web block")
