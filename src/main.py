6f89e196bc7c0eb6fa50c8cf91b4ff1212196a62
Joaquin Welch <joaquinwelch510@gmail.com>
2024-04-28 15:58:29 -0500

    added blocked websites

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-window = Tk()
-heading=Label(window, text ='Website Blocker' , font ='arial')
-heading.pack()
-window.geometry('650x400')
-window.minsize(650,400)
-window.maxsize(650,400)
-window.title("web block")
